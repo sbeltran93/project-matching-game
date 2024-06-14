31613761d400e1b43c73946d2e41a32874993ca3
Sammy <samanthabeltran1993@gmail.com>
2024-06-13 18:34:47 -0700

    almost completely functioning

--- js/app.js
+++ js/app.js
@@ -23,21 +23,17 @@ const matchCountEl = document.querySelector('.matches')
 const startButtonEl = document.querySelector('#start')
 
 /*-------------- Functions -------------*/
-function init() {
-    deck = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'];
-    cards = [];
-    firstCard = 0
-    secondCard = 0
-    matches = '';
-    winner = false;    
+function init() {       
 shuffleDeck()
-// shuffle(deck)
-  cardsEl.forEach((card) => {
-    card.innerText = deck[card]
-}) 
+display = document.querySelector(".time");
+    StartTimer(90, display);
+    matchCountEl.textContent = 0
 }
-
 const render = () => {
+}
+function winningGame(){
+    if (matchCountEl.textContent === 2)
+        messageEl.textContent= `Congratulations winner! You got all the matches!`
 }
 
 function shuffleDeck () {
@@ -51,50 +47,71 @@ function shuffleDeck () {
     // console.log(deck)
         deck.forEach((card, i) => {
         cardsEl[i].textContent = card;
+        cardsEl[i].style.color = 'rgba(0, 0, 0, 0)'
     }) 
 };
-function cardFlip () {
-     
-    }   
-
+ 
 /*----------- Event Listeners ----------*/
 function onClick(event) {
-cardIdx = event.target.id
-render()
-// init()
-// shuffleDeck()
-// console.log("clicked", cardIdx)
+    cardIdx = event.target.id
+    render()
 }
-// boardEl.addEventListener("click")
+startButtonEl.addEventListener('click', init)
+
+resetBtnEl.addEventListener('click', init)
 
 cardsEl.forEach((card) => {
     card.addEventListener('click', onClick)
     render()
 });
     boardEl.addEventListener('click', (evt) => {
+        evt.target.style.color = 'rgba(0, 0, 0, 1)'
         if (!firstCard && !secondCard) {
-            firstCard = evt.target.textContent
+            firstCard = evt.target
         }   else if (firstCard && !secondCard) {
-            secondCard = evt.target.textContent
-        if (firstCard === secondCard) {
+            secondCard = evt.target
+        if (firstCard.textContent === secondCard.textContent) {
             matchCountEl.textContent  ++;
+            match = true
             messageEl.textContent= `You have a match!`
-            
-            // console.log("You have a match!")
+            firstCard = null
+            secondCard = null
         }   else {
             messageEl.textContent= `Not a match, try again!`
-            // console.log("not a match, try again!")
-        }
-        firstCard = null
-        secondCard = null
-        // setTimeout(hideCards, 1000)
+            setTimeout(()=> {
+                firstCard.style.color = 'rgba(0, 0, 0, 0)'
+                secondCard.style.color = 'rgba(0, 0, 0, 0)'
+                firstCard = null
+                secondCard = null
+            }, 200)
+            messageEl.textContent= `Not a match, try again!`    
+        }   
         }})
-        console.log("First Card: " + firstCard, "Second Card: " + secondCard)
 
+function StartTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    const interval = setInterval(function(){
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-startButtonEl.addEventListener('click', shuffleDeck)
-    
+            minutes =  minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
 
-resetBtnEl.addEventListener('click', render)
+            display.textContent = minutes + ":" + seconds;
 
+            if(--timer < 0) {
+                timer = duration;
+               clearInterval(interval)
+               messageEl.textContent= `Times up! You lose! Try again?`
+             } 
+        // create function to stop game and invoke in if statement
+        // message for timer runs out
 
+    }, 1000);
+}
+
+// https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
+function winningGame(){
+    if (matchCountEl.textContent === 2)
+        messageEl.textContent= `Congratulations winner! You got all the matches!`
+}
