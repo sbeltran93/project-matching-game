4930a5b33202ea6fd115319e0d71e30cd29afe0f
Sammy <samanthabeltran1993@gmail.com>
2024-06-14 15:45:22 -0700

    just need one more function for win

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # matching game
+
+
+![](2024-06-14-15-09-50.png)
+
+
+My game is a number matching game, with 8 matches to get and a timer of 1:30.
+If you get all 8 matches in 1:30, you win! If not, you lose, try again!
+
+I chose this game because my daughter loves games and I thought it would be cool for her to be able to play a game I built. We took turns as if it was a 2 player game and we had fun! Also, it seemed like quite a challenge, but not too challenging for me to be lost the whole time. It was much harder than I was expecting though. I enjoyed making this game.
+
+
+Links to attributions: sites I used to help build my game. Not sure if this is what this section means..
+
+https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
+https://www.youtube.com/watch?v=xWdkt6KSirw&t=449s
+https://www.youtube.com/watch?v=wz9jeI9M9hI
+https://marketplace.visualstudio.com/items?itemName=mushan.vscode-paste-image
+
+Technologies used:
+HTML
+CSS
+JavaScript
+gitHub
+Youtube
+webmdn
+stackoverflow
+
+
+For my future enhancments I would like to when you push the start button, the timer would count correctly. Also, when you click near a card, but not on it, it acts a little wierd. Not sure how you could fix that at my level of experience..Add pictures onto the cards instead of just numbers.
--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 /*-------------- Constants -------------*/
+const render = () => {
+}
 
 /*---------- Variables (state) ---------*/
 let deck = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'];
 let cards = [];
-let firstCard = 0
-let secondCard = 0
+let firstCard = 0;
+let secondCard = 0;
 let matches = 0;
-let winner = false;
-// let shuffledDeck = []
+let winner = "";
 
 /*----- Cached Element References  -----*/
 const boardEl = document.querySelector(".deck");
@@ -29,64 +30,23 @@ display = document.querySelector(".time");
     StartTimer(90, display);
     matchCountEl.textContent = 0
 }
-const render = () => {
-}
 function winningGame(){
-    if (matchCountEl.textContent === 2)
+    if (matchCountEl == '2')
         messageEl.textContent= `Congratulations winner! You got all the matches!`
 }
-
+   
 function shuffleDeck () {
-//    console.log(deck)
     for (let i = 0; i < deck.length ; i++) {
         let shuffle = Math.floor(Math.random() * (deck.length))
         let temp = deck[i];
         deck[i] = deck[shuffle];
         deck[shuffle] = temp;  
     } 
-    // console.log(deck)
         deck.forEach((card, i) => {
         cardsEl[i].textContent = card;
         cardsEl[i].style.color = 'rgba(0, 0, 0, 0)'
     }) 
 };
- 
-/*----------- Event Listeners ----------*/
-function onClick(event) {
-    cardIdx = event.target.id
-    render()
-}
-startButtonEl.addEventListener('click', init)
-
-resetBtnEl.addEventListener('click', init)
-
-cardsEl.forEach((card) => {
-    card.addEventListener('click', onClick)
-    render()
-});
-    boardEl.addEventListener('click', (evt) => {
-        evt.target.style.color = 'rgba(0, 0, 0, 1)'
-        if (!firstCard && !secondCard) {
-            firstCard = evt.target
-        }   else if (firstCard && !secondCard) {
-            secondCard = evt.target
-        if (firstCard.textContent === secondCard.textContent) {
-            matchCountEl.textContent  ++;
-            match = true
-            messageEl.textContent= `You have a match!`
-            firstCard = null
-            secondCard = null
-        }   else {
-            messageEl.textContent= `Not a match, try again!`
-            setTimeout(()=> {
-                firstCard.style.color = 'rgba(0, 0, 0, 0)'
-                secondCard.style.color = 'rgba(0, 0, 0, 0)'
-                firstCard = null
-                secondCard = null
-            }, 200)
-            messageEl.textContent= `Not a match, try again!`    
-        }   
-        }})
 
 function StartTimer(duration, display) {
     let timer = duration, minutes, seconds;
@@ -105,13 +65,63 @@ function StartTimer(duration, display) {
                messageEl.textContent= `Times up! You lose! Try again?`
              } 
         // create function to stop game and invoke in if statement
-        // message for timer runs out
 
     }, 1000);
 }
+ 
+/*----------- Event Listeners ----------*/
+function onClick(event) {
+    cardIdx = event.target.id
+    render()
+}
+startButtonEl.addEventListener('click', init)
 
-// https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
+resetBtnEl.addEventListener('click', init)
+
+matchCountEl.addEventListener('click', (evt) => {
+    if (matchCountEl == 2)
+         return messageEl.textContent= `Winner!`
+})
+
+cardsEl.forEach((card) => {
+    card.addEventListener('click', onClick)
+    render()
+});
+    boardEl.addEventListener('click', (evt) => {
+        evt.target.style.color = 'rgba(0, 0, 0, 1)'
+        if (!firstCard && !secondCard) {
+            firstCard = evt.target
+        }   else if (firstCard && !secondCard) {
+            secondCard = evt.target
+        if (firstCard.textContent === secondCard.textContent) {
+            matchCountEl.textContent  ++;
+            messageEl.textContent= `You have a match!`
+            firstCard = null
+            secondCard = null
+        }   else {
+            messageEl.textContent= `Not a match, try again!`
+            setTimeout(()=> {
+                firstCard.style.color = 'rgba(0, 0, 0, 0)'
+                secondCard.style.color = 'rgba(0, 0, 0, 0)'
+                firstCard = null
+                secondCard = null
+            }, 200)
+            // messageEl.textContent= `Not a match, try again!`    
+        }   
+        }})
+
+/*
 function winningGame(){
     if (matchCountEl.textContent === 2)
         messageEl.textContent= `Congratulations winner! You got all the matches!`
 }
+
+function winningGame(){
+    if (matchCountEl = parseInt("1")     
+        messageEl.textContent= `Congratulations winner! You got all the matches!`
+})
+
+
+    */
+
+
